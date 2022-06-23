12fd597a023a40365faf07ee1e28b5cf0fd6b9e3
OviyaGunasekaran <ovybusybee@gmail.com>
2022-06-24 00:09:28 +0530

    Java 1)DataTypeSize Pgm added

---
+++ DatatypeSize.java
@@ -0,0 +1,39 @@
+package Oviya;
+
+public class DataTypesimple {
+	
+			public static void main(String args[])
+			{   boolean boo1 = true;
+			 	boolean boo2 = false;
+					 	
+			 	System.out.println(boo1+" "+boo2); 
+				
+			 
+			 	byte b = 126;                                               
+				 
+		      	System.out.println(b);                        					 
+		        b++;
+		        System.out.println(b);	 
+		      	b++;														
+		      	System.out.println(b);										  						
+		      	b++;															        System.out.println(b);
+				
+				char a = 'G';												
+				int i = 89;													
+
+				byte b1 = 4;												
+				short s = 56;							
+
+				double d = 4.355453532;										
+				float f = 4.7333434f;												
+				long l = 12121; 															
+				System.out.println("char: " + a);
+				System.out.println("integer: " + i);
+				System.out.println("byte: " + b1);
+				System.out.println("short: " + s);
+				System.out.println("float: " + f);
+				System.out.println("double: " + d);
+				System.out.println("long: " + l);
+			}
+
+}
