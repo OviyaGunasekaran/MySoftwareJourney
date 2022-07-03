e6fb32ec8a08fbd0d2c7bdc84f1e107cb92176f4
OviyaGunasekaran <ovybusybee@gmail.com>
2022-07-03 19:43:04 +0530

    Java:[5]Fibonacci Series - User input via Scanner usage

--- FibonacciScannerUsage.java
+++ FibonacciScannerUsage.java
@@ -1,18 +1,22 @@
 package Oviya;
 import java.util.Scanner;
 
-public class FinonacciScannerUsage {
+public class FibonacciScannerUsage {
 	
 	public static void main(String args[])  
 	{    
 	 int n1=0,n2=1,n3,i;
 	 
 	 Scanner sc= new Scanner(System.in);
-	 System.out.print("How many numbers you wish to see in Fibonacci Series?");
+	 System.out.print("How many numbers you wish to see in Fibonacci Series? ");
 	 
 	 int count = sc.nextInt();
 	 System.out.print("First "+count+" numbers of Fibonacci Series:\n");   
 	
+	if (count == 1)
+		System.out.print(n1); 
+	else
+		
 	 System.out.print(n1+" "+n2);   
 	    
 	 for(i=2;i<count;i++)   
@@ -26,3 +30,4 @@ public class FinonacciScannerUsage {
 	}
 
 }
+
