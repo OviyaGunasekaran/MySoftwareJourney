7527074dff8a26419d4be8221453cc1c30b075d7
OviyaGunasekaran <ovybusybee@gmail.com>
2022-07-05 21:57:34 +0530

    ReactJS-Default Export (app.JS file and Output) added

---
+++ React3DefaultExportJS_File.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	 
+  let name = "OVIYA";
+  return (
+  
+  <div className="App">
+		<h1> Welcome to React </h1>
+		<h1> {name}	</h1>
+  </div>
+    
+  );
+}
+
+export default App;
