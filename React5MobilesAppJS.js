578f9f57c6e4533ae2669fcedbb63a132f91a238
OviyaGunasekaran <ovybusybee@gmail.com>
2022-07-06 22:43:05 +0530

    ReactJS-Mobiles App(.jsx,outputs,app.js,indexhtml,indexjs added)

---
+++ React5MobilesAppJS.js
@@ -0,0 +1,19 @@
+import "./App.css"; 
+import Mobiles from "./component/Mobiles"
+
+function App() {
+	 
+  return (
+   
+  <div className="App">
+  <h1> Check App.js </h1>
+  <Mobiles></Mobiles>
+  <Mobiles/>
+  <Mobiles></Mobiles>
+  </div>
+    
+  );
+}
+
+export default App;
+
